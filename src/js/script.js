2d9b122a08dfa5ff1df1355badfecf0d75a03b6c
Vladislava Soina <soina9797@gmail.com>
2021-09-26 18:58:42 +0300

    WMF_1

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,25 @@
-const elements = document.querySelectorAll('a');
+// active
+const elements = document.querySelectorAll('.nav__link');
 
 elements.forEach((i) => {
     i.addEventListener('click', function (e) {
-        e.preventDefault();
-        const active = document.querySelector('a.active');
+        const active = document.querySelector('.nav__link.active');
         if (active) {
             active.classList.remove('active');
         }
         e.target.classList.add('active');
     });
+});
+
+// hamburger 
+const hamburger = document.querySelector('.header__hamburger'),
+      sidebar = document.querySelector('.hub-page__sidebar'),
+      close = document.querySelector('.searchpanel__close');
+
+hamburger.addEventListener('click', () => {
+    sidebar.classList.add('active-sidebar');
+});
+
+close.addEventListener('click', () => {
+    sidebar.classList.remove('active-sidebar');
 });
